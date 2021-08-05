ecf360a268151fab8a88398cbc96977ac852aab3
AndyZe <zelenak@picknik.ai>
2021-08-05 08:15:45 -0500

    Only include <ros2_control> xacro stuff once

--- kortex2_bringup/launch/kortex_control.launch.py
+++ kortex2_bringup/launch/kortex_control.launch.py
@@ -237,7 +237,7 @@ def generate_launch_description():
         robot_state_publisher_node,
         rviz_node,
         joint_state_broadcaster_spawner,
-        #        robot_controller_spawner,
+        robot_controller_spawner,
         robot_hand_controller_spawner,
     ]
 
