9743fb0844923d6f8a02b064d3854841a1bc878c
AndyZe <zelenak@picknik.ai>
2021-08-19 14:22:10 -0500

    Add a mostly-empty kortex2_controllers package

---
+++ kortex2_controllers/include/kortex2_controllers/twist_controller.hpp
@@ -0,0 +1,49 @@
+// Copyright 2020 PAL Robotics S.L.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "forward_command_controller/forward_command_controller.hpp"
+#include "kortex2_controllers/visibility_control.h"
+#include "rclcpp_lifecycle/node_interfaces/lifecycle_node_interface.hpp"
+#include "rclcpp_lifecycle/state.hpp"
+#include "rclcpp/subscription.hpp"
+#include "realtime_tools/realtime_buffer.h"
+#include "geometry_msgs/msg/twist.hpp"
+
+namespace kortex2_controllers
+{
+using CallbackReturn = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
+
+/**
+ * \brief Forward a twist command to the proprietary Kinova servoing API
+ *
+ * This is similar functionality to MoveIt Servo, but Kinova does not expose the low-level controllers in a way
+ * that works well with Servo.
+ *
+ * \param joints Names of the joints to control.
+ * \param interface_name Name of the interface to command.
+ *
+ * Subscribes to:
+ * - \b commands (geometry_msgs::msg::Twist) : The commands to apply in frame X by default.
+ */
+class TwistController : public forward_command_controller::ForwardCommandController
+{
+};
+
+}  // namespace kortex2_controllers
---
+++ kortex2_controllers/src/twist_controller.cpp
@@ -0,0 +1,23 @@
+// Copyright 2020 PAL Robotics S.L.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "kortex2_controllers/twist_controller.hpp"
+
+namespace kortex2_controllers
+{
+}  // namespace kortex2_controllers
+
+#include "pluginlib/class_list_macros.hpp"
+
+PLUGINLIB_EXPORT_CLASS(kortex2_controllers::TwistController, controller_interface::ControllerInterface)
