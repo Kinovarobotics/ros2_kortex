b7a4952dd572ba99b3ca9de7dc4024873c7a77b9
Bren Pierce <bren@brenpierce.com>
2021-08-20 13:31:11 +0000

    removed un-used testing files

--- kortex2_robotiq_gripper_driver/setup.py
+++ kortex2_robotiq_gripper_driver/setup.py
@@ -1,25 +1,21 @@
 from setuptools import setup
 
-package_name = 'kortex2_robotiq_gripper_driver'
+package_name = "kortex2_robotiq_gripper_driver"
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version="0.0.0",
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='bren',
-    maintainer_email='bren@brenpierce.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    maintainer="brennand pierce",
+    maintainer_email="bren.pierce@picknik.ai",
     entry_points={
-        'console_scripts': [
-            'robotiq_gripper_driver_85_action_server = kortex2_robotiq_gripper_driver.robotiq_gripper_driver_85_action_server:main',
+        "console_scripts": [
+            "robotiq_gripper_driver_85_action_server = kortex2_robotiq_gripper_driver.robotiq_gripper_driver_85_action_server:main",
         ],
     },
 )
