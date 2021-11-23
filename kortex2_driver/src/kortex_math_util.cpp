ce1813c4e3742722fe8e50da346b15202085f0b5
Denis Štogl <denis@stogl.de>
2021-11-23 15:51:01 +0100

    Update math utils license.

--- kortex2_driver/src/kortex_math_util.cpp
+++ kortex2_driver/src/kortex_math_util.cpp
@@ -1,10 +1,8 @@
-/*
- * Copyright (c) 2019 Kinova inc. All rights reserved.
- *
- * Use of this source code is governed by a BSD-style
- * license that can be found in the LICENSE file or at
- * https://developers.google.com/open-source/licenses/bsd
- */
+// Copyright (c) 2019 Kinova inc. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file or at
+// https://developers.google.com/open-source/licenses/bsd
 
 #include "kortex2_driver/kortex_math_util.hpp"
 
