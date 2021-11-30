2478db2768e8e58951a88c4605cf91933acf1a7b
Denis Štogl <denis@stogl.de>
2021-11-30 18:40:58 +0100

    Adjust URDF and launch files for Gazebo and Ignition simulations.

--- kortex2_bringup/launch/kortex_sim_control.launch.py
+++ kortex2_bringup/launch/kortex_sim_control.launch.py
@@ -15,7 +15,8 @@
 # Author: Marq Rasmussen
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.conditions import IfCondition
 from launch.substitutions import (
     Command,
@@ -29,53 +30,28 @@ from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
     declared_arguments = []
+    # Simulation specific arguments
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "ignition_sim",
+            default_value="true",
+            description="Use Ignition for simulation",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "gazebo_sim",
+            default_value="false",
+            description="Use Gazebo Classic for simulation",
+        )
+    )
     # Robot specific arguments
     declared_arguments.append(
         DeclareLaunchArgument(
-            "robot_type", description="Type/series of robot.", choices=["gen3", "gen3_lite"]
-        )
-    )
-    # TODO(anyone): enable this
-    # choices=['gen3', 'gen3_lite', ...]))
-    declared_arguments.append(
-        DeclareLaunchArgument(
-            "robot_ip", description="IP address by which the robot can be reached."
-        )
-    )
-    declared_arguments.append(
-        DeclareLaunchArgument(
-            "username", description="Robot session username.", default_value="admin"
-        )
-    )
-    declared_arguments.append(
-        DeclareLaunchArgument(
-            "password", description="Robot session password.", default_value="admin"
-        )
-    )
-    declared_arguments.append(
-        DeclareLaunchArgument(
-            "port", description="Robot port for tcp connection.", default_value="10000"
-        )
-    )
-    declared_arguments.append(
-        DeclareLaunchArgument(
-            "port_realtime",
-            description="Robot port for udp realtime control.",
-            default_value="10001",
-        )
-    )
-    declared_arguments.append(
-        DeclareLaunchArgument(
-            "session_inactivity_timeout_ms",
-            description="Robot session inactivity timeout in milliseconds.",
-            default_value="60000",
-        )
-    )
-    declared_arguments.append(
-        DeclareLaunchArgument(
-            "connection_inactivity_timeout_ms",
-            description="Robot connection inactivity timeout in milliseconds.",
-            default_value="2000",
+            "robot_type",
+            description="Type/series of robot.",
+            choices=["gen3", "gen3_lite"],
+            default_value="gen3",
         )
     )
     # General arguments
@@ -104,6 +80,13 @@ def generate_launch_description():
     )
     declared_arguments.append(
         DeclareLaunchArgument(
+            "description_file",
+            default_value="kinova.urdf.xacro",
+            description="URDF/XACRO description file with the robot.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
             "moveit_config_package",
             default_value="gen3_robotiq_2f_85_move_it_config",
             description="MoveIt configuration package for the robot. Usually the argument \
@@ -112,9 +95,9 @@ def generate_launch_description():
     )
     declared_arguments.append(
         DeclareLaunchArgument(
-            "description_file",
-            default_value="kinova.urdf.xacro",
-            description="URDF/XACRO description file with the robot.",
+            "name",
+            default_value="kinova",
+            description="Robot name.",
         )
     )
     declared_arguments.append(
@@ -124,28 +107,6 @@ def generate_launch_description():
             description="Prefix of the joint names, useful for \
         multi-robot setup. If changed than also joint names in the controllers' configuration \
         have to be updated.",
-        )
-    )
-    declared_arguments.append(
-        DeclareLaunchArgument(
-            "gripper",
-            default_value="robotiq_2f_85",
-            description="Name of the gripper attached to the arm",
-        )
-    )
-    declared_arguments.append(
-        DeclareLaunchArgument(
-            "use_fake_hardware",
-            default_value="false",
-            description="Start robot with fake hardware mirroring command to its states.",
-        )
-    )
-    declared_arguments.append(
-        DeclareLaunchArgument(
-            "fake_sensor_commands",
-            default_value="false",
-            description="Enable fake command interfaces for sensors used for simple simulations. \
-            Used only if 'use_fake_hardware' parameter is true.",
         )
     )
     declared_arguments.append(
@@ -170,42 +131,33 @@ def generate_launch_description():
         )
     )
     declared_arguments.append(
-        DeclareLaunchArgument(
-            "fault_controller",
-            default_value="fault_controller",
-            description="Name of the 'fault controller.",
-        )
-    )
-    declared_arguments.append(
         DeclareLaunchArgument("launch_rviz", default_value="true", description="Launch RViz?")
-    )
-    declared_arguments.append(
-        DeclareLaunchArgument(
-            "use_internal_bus_gripper_comm",
-            default_value="true",
-            description="Use internal bus for gripper communication?",
-        )
     )
 
     # Initialize Arguments
+    gazebo_sim = LaunchConfiguration("gazebo_sim")
+    ignition_sim = LaunchConfiguration("ignition_sim")
     robot_type = LaunchConfiguration("robot_type")
-    robot_ip = LaunchConfiguration("robot_ip")
     # General arguments
     runtime_config_package = LaunchConfiguration("runtime_config_package")
     controllers_file = LaunchConfiguration("controllers_file")
     description_package = LaunchConfiguration("description_package")
-    moveit_config_package = LaunchConfiguration("moveit_config_package")
     description_file = LaunchConfiguration("description_file")
+    moveit_config_package = LaunchConfiguration("moveit_config_package")
+    name = LaunchConfiguration("name")
     prefix = LaunchConfiguration("prefix")
-    gripper = LaunchConfiguration("gripper")
-    use_fake_hardware = LaunchConfiguration("use_fake_hardware")
-    fake_sensor_commands = LaunchConfiguration("fake_sensor_commands")
     robot_traj_controller = LaunchConfiguration("robot_controller")
     robot_pos_controller = LaunchConfiguration("robot_pos_controller")
     robot_hand_controller = LaunchConfiguration("robot_hand_controller")
-    fault_controller = LaunchConfiguration("fault_controller")
     launch_rviz = LaunchConfiguration("launch_rviz")
-    use_internal_bus_gripper_comm = LaunchConfiguration("use_internal_bus_gripper_comm")
+
+    robot_controllers = PathJoinSubstitution(
+        [FindPackageShare(runtime_config_package), "config", controllers_file]
+    )
+
+    rviz_config_file = PathJoinSubstitution(
+        [FindPackageShare(description_package), "rviz", "urdf_description.rviz"]
+    )
 
     robot_description_content = Command(
         [
@@ -215,11 +167,10 @@ def generate_launch_description():
                 [FindPackageShare(description_package), "robots", description_file]
             ),
             " ",
-            "robot_ip:=",
-            robot_ip,
+            "robot_ip:=xxx.yyy.zzz.www",
             " ",
             "name:=",
-            robot_type,
+            name,
             " ",
             "arm:=",
             robot_type,
@@ -227,39 +178,18 @@ def generate_launch_description():
             "prefix:=",
             prefix,
             " ",
-            "use_fake_hardware:=",
-            use_fake_hardware,
+            "gazebo_sim:=",
+            gazebo_sim,
             " ",
-            "fake_sensor_commands:=",
-            fake_sensor_commands,
+            "ignition_sim:=",
+            ignition_sim,
             " ",
-            "gripper:=",
-            gripper,
-            " ",
-            "use_internal_bus_gripper_comm:=",
-            use_internal_bus_gripper_comm,
+            "gazebo_controllers:=",
+            robot_controllers,
             " ",
         ]
     )
     robot_description = {"robot_description": robot_description_content}
-
-    robot_controllers = PathJoinSubstitution(
-        [FindPackageShare(runtime_config_package), "config", controllers_file]
-    )
-
-    rviz_config_file = PathJoinSubstitution(
-        [FindPackageShare(moveit_config_package), "rviz", "moveit.rviz"]
-    )
-
-    control_node = Node(
-        package="controller_manager",
-        executable="ros2_control_node",
-        parameters=[robot_description, robot_controllers],
-        output={
-            "stdout": "screen",
-            "stderr": "screen",
-        },
-    )
 
     robot_state_publisher_node = Node(
         package="robot_state_publisher",
@@ -270,11 +200,11 @@ def generate_launch_description():
 
     rviz_node = Node(
         package="rviz2",
-        condition=IfCondition(launch_rviz),
         executable="rviz2",
         name="rviz2",
         output="log",
         arguments=["-d", rviz_config_file],
+        condition=IfCondition(launch_rviz),
     )
 
     joint_state_broadcaster_spawner = Node(
@@ -305,32 +235,58 @@ def generate_launch_description():
         arguments=[robot_hand_controller, "-c", "/controller_manager"],
     )
 
-    fault_controller_spawner = Node(
-        package="controller_manager",
-        executable="spawner.py",
-        arguments=[fault_controller, "-c", "/controller_manager"],
+    # Gazebo nodes
+    gzserver = ExecuteProcess(
+        cmd=["gzserver", "-s", "libgazebo_ros_init.so", "-s", "libgazebo_ros_factory.so", ""],
+        output="screen",
+        condition=IfCondition(gazebo_sim),
     )
 
-    robotiq_gripper_server = Node(
-        package="kortex2_robotiq_gripper_driver",
-        executable="robotiq_gripper_driver_85_action_server",
-        condition=IfCondition(use_internal_bus_gripper_comm),
-        output={
-            "stdout": "screen",
-            "stderr": "screen",
-        },
+    # Gazebo client
+    gzclient = ExecuteProcess(
+        cmd=["gzclient"],
+        output="screen",
+        condition=IfCondition(gazebo_sim),
+    )
+
+    # Spawn robot
+    gazebo_spawn_robot = Node(
+        package="gazebo_ros",
+        executable="spawn_entity.py",
+        name="spawn_ur",
+        arguments=["-entity", name, "-topic", "robot_description"],
+        output="screen",
+        condition=IfCondition(gazebo_sim),
+    )
+
+    ignition_spawn_entity = Node(
+        package="ros_ign_gazebo",
+        executable="create",
+        output="screen",
+        arguments=["-string", robot_description_content, "-name", name, "-allow_renaming", "true"],
+        condition=IfCondition(ignition_sim),
+    )
+
+    ignition_launch_description = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [FindPackageShare("ros_ign_gazebo"), "/launch/ign_gazebo.launch.py"]
+        ),
+        launch_arguments={"ign_args": " -r -v 3 empty.sdf"}.items(),
+        condition=IfCondition(ignition_sim),
     )
 
     nodes_to_start = [
-        control_node,
         robot_state_publisher_node,
         rviz_node,
         joint_state_broadcaster_spawner,
         robot_traj_controller_spawner,
         robot_pos_controller_spawner,
-        robot_hand_controller_spawner,
-        robotiq_gripper_server,
-        fault_controller_spawner,
+        # robot_hand_controller_spawner,
+        gzserver,
+        gzclient,
+        gazebo_spawn_robot,
+        ignition_launch_description,
+        ignition_spawn_entity,
     ]
 
     return LaunchDescription(declared_arguments + nodes_to_start)
