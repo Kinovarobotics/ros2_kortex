f4f7cf85c7757a3967eb9528fbe2537d433f9e88
Anthony Baker <ABake48@gmail.com>
2023-05-11 12:33:04 -0600

    pre-commit

--- kortex_moveit_config/kinova_gen3_7dof_robotiq_2f_85_moveit_config/launch/warehouse_db.launch.py
+++ kortex_moveit_config/kinova_gen3_7dof_robotiq_2f_85_moveit_config/launch/warehouse_db.launch.py
@@ -18,5 +18,7 @@ from moveit_configs_utils.launches import generate_warehouse_db_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("gen3", package_name="kinova_gen3_7dof_robotiq_2f_85_moveit_config").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder(
+        "gen3", package_name="kinova_gen3_7dof_robotiq_2f_85_moveit_config"
+    ).to_moveit_configs()
     return generate_warehouse_db_launch(moveit_config)
