5fe26fba6dd9884775db070fd811752043b037b6
Anthony Baker <abake48@gmail.com>
2023-05-30 14:22:09 -0600

    remove unused var

--- kortex2_bringup/launch/kortex_control.launch.py
+++ kortex2_bringup/launch/kortex_control.launch.py
@@ -81,15 +81,6 @@ def generate_launch_description():
     # General arguments
     declared_arguments.append(
         DeclareLaunchArgument(
-            "runtime_config_package",
-            default_value="kortex2_bringup",
-            description='Package with the controller\'s configuration in "config" folder. \
-        Usually the argument is not set, it enables use of a custom setup.',
-        )
-    )
-
-    declared_arguments.append(
-        DeclareLaunchArgument(
             "controllers_file",
             default_value="ros2_controllers.yaml",
             description="YAML file with the controllers configuration.",
@@ -192,7 +183,6 @@ def generate_launch_description():
     robot_ip = LaunchConfiguration("robot_ip")
     dof = LaunchConfiguration("dof")
     # General arguments
-    runtime_config_package = LaunchConfiguration("runtime_config_package")
     controllers_file = LaunchConfiguration("controllers_file")
     description_package = LaunchConfiguration("description_package")
     description_file = LaunchConfiguration("description_file")
