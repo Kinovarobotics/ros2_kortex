68ec24b33783990c464b5aeb51db384b4d445ef4
Sebastian Jahr <sebastian.jahr@picknik.ai>
2023-12-06 14:53:01 -0700

    Update planner_plugin parameters

--- kortex_moveit_config/launch/kortex_moveit.launch.py
+++ kortex_moveit_config/launch/kortex_moveit.launch.py
@@ -211,7 +211,7 @@ def generate_launch_description():
     # Planning Configuration
     ompl_planning_pipeline_config = {
         "move_group": {
-            "planning_plugin": "ompl_interface/OMPLPlanner",
+            "planning_plugins": ["ompl_interface/OMPLPlanner"],
             "request_adapters": """default_planner_request_adapters/AddTimeOptimalParameterization default_planner_request_adapters/FixWorkspaceBounds default_planner_request_adapters/FixStartStateBounds default_planner_request_adapters/FixStartStateCollision default_planner_request_adapters/FixStartStatePathConstraints""",
             "start_state_max_bounds_error": 0.1,
         }
