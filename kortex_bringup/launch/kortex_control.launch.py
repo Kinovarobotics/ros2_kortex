2311af3ab2aaaf72ca703f271e6923030f1c42df
Sebastian Jahr <sebastian.jahr@picknik.ai>
2023-12-19 19:06:31 +0100

    Update ros2_control usage to recent deprecations

--- kortex_bringup/launch/kortex_control.launch.py
+++ kortex_bringup/launch/kortex_control.launch.py
@@ -123,7 +123,10 @@ def launch_setup(context, *args, **kwargs):
     control_node = Node(
         package="controller_manager",
         executable="ros2_control_node",
-        parameters=[robot_description, robot_controllers],
+        parameters=[robot_controllers],
+        remappings=[
+            ("/controller_manager/robot_description", "/robot_description"),
+        ],
         output="both",
     )
 
