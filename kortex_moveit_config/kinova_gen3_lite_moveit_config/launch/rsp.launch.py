dc84f275a835a120326efcfca41f4dd93a391a89
Abed Al Rahman Al Mrad <aalmrad@kinova.ca>
2024-08-01 11:22:05 -0400

    Format changes

--- kortex_moveit_config/kinova_gen3_lite_moveit_config/launch/rsp.launch.py
+++ kortex_moveit_config/kinova_gen3_lite_moveit_config/launch/rsp.launch.py
@@ -3,5 +3,7 @@ from moveit_configs_utils.launches import generate_rsp_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("gen3_lite", package_name="kinova_gen3_lite_moveit_config").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder(
+        "gen3_lite", package_name="kinova_gen3_lite_moveit_config"
+    ).to_moveit_configs()
     return generate_rsp_launch(moveit_config)
--- kortex_moveit_config/kinova_gen3_lite_moveit_config/launch/static_virtual_joint_tfs.launch.py
+++ kortex_moveit_config/kinova_gen3_lite_moveit_config/launch/static_virtual_joint_tfs.launch.py
@@ -3,5 +3,7 @@ from moveit_configs_utils.launches import generate_static_virtual_joint_tfs_laun
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("gen3_lite", package_name="kinova_gen3_lite_moveit_config").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder(
+        "gen3_lite", package_name="kinova_gen3_lite_moveit_config"
+    ).to_moveit_configs()
     return generate_static_virtual_joint_tfs_launch(moveit_config)
