80dde3fdc2f4a30ae0adde98a9b2c59ef52d0fce
Abed Al Rahman Al Mrad <aalmrad@kinova.ca>
2025-11-12 16:37:22 -0500

    format rectification

--- kortex_moveit_config/kinova_gen3_lite_moveit_config/launch/sim.launch.py
+++ kortex_moveit_config/kinova_gen3_lite_moveit_config/launch/sim.launch.py
@@ -82,7 +82,7 @@ def generate_launch_description():
             publish_planning_scene=True,
             publish_geometry_updates=True,
             publish_state_updates=True,
-            publish_transforms_updates=True
+            publish_transforms_updates=True,
         )
         .planning_pipelines(pipelines=["ompl", "pilz_industrial_motion_planner"])
         .to_moveit_configs()
